ed13e761965811c7ae898692d06ea3bc8e7fc0ae
Torah Oglander <torahlight@gmail.com>
2023-02-21 13:00:08 -0600

    update code in sequence project

--- sequence/lib/sequence/server.ex
+++ sequence/lib/sequence/server.ex
@@ -1,6 +1,25 @@
 defmodule Sequence.Server do
   use GenServer
 
+  ################
+  # External API
+  def start_link(current_number) do
+    GenServer.start_link(
+      __MODULE__,
+      current_number,
+      name: __MODULE__
+    )
+  end
+
+  def next_number do
+    GenServer.call __MODULE__, :next_number
+  end
+
+  def set_number(new_number) do
+    GenServer.cast __MODULE__, { :set_number, new_number }
+  end
+  ################
+
   def init(initial_number) do
     { :ok, initial_number }
   end
@@ -9,7 +28,7 @@ defmodule Sequence.Server do
     { :reply, current_number, current_number + 1 }
   end
 
-  def handle_call({ :set_number, new_number }, _from, _current_number) do
-    { :reply, new_number, new_number }
+  def handle_cast({ :set_number, new_number }, _current_number) do
+    { :noreply, new_number }
   end
 end
